cb7f7416cabe3a5b054ee754a844690f3d41485b
MattiWheels <lime811@gmail.com>
2022-11-30 19:50:13 -0800

    added flask-bootstrap (bootstrap3)

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,6 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-def based():
-    return render_template('base.html')
-
 @app.route('/index')
 def index():
     return render_template('index.html')
